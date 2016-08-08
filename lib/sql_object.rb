dd62948d29f0589ca68ca16ebee50abd8fa5354a
Timothy Callahan <tim.p.callahan@gmail.com>
2016-08-08 18:38:26 -0400

    sql object table name, columns, attrs, initialize

--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -1,2 +1,52 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+
+class SQLObject
+  def self.table_name
+    @table_name || self.to_s.tableize
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.columns
+    return @columns if @columns
+
+    result = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+       #{self.table_name}
+    SQL
+
+    @columns = result.first.map(&:to_sym)
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name)
+        raise "unknown attribute '#{attr_name}'"
+      end
+      
+      attr_name = attr_name.to_sym
+        self.send("#{attr_name}=", value)
+    end
+  end
+end
